8f3c191c67b2da52c2c9d09cefa6cfa34312f513
HOLYPROF <71444357+holyprof1@users.noreply.github.com>
2025-12-13 15:49:20 +0100

    Fix syntax error in again.php

--- routes/again.php
+++ routes/again.php
@@ -1,7 +1,6 @@
 <?php
 // wp-content/themes/savvy-media/functions.php
   add_theme_support('post-thumbnails');
-
 });
 add_action('wp_enqueue_scripts', function(){
   wp_enqueue_style('savvy-style', get_stylesheet_uri(), [], '1.0');
