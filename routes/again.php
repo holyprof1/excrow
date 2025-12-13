03ec1ac5c5a351a48700e409163e1c35282f0738
HOLYPROF <71444357+holyprof1@users.noreply.github.com>
2025-12-13 15:48:36 +0100

    Update again.php

--- routes/again.php
+++ routes/again.php
@@ -6,6 +6,5 @@
 });
 add_action('wp_enqueue_scripts', function(){
   wp_enqueue_style('savvy-style', get_stylesheet_uri(), [], '1.0');
-  wp_enqueue_script('savvy-main', get_template_directory_uri().'/assets/js/main.js', ['jquery'], '1.0', true);
 });
 ?>
